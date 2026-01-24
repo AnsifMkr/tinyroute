e914aac70fdd95aa649d3440821bd9e1c097864e
Ansif <99172928+AnsifMkr@users.noreply.github.com>
2026-01-24 10:47:30 +0530

    Add OPTIONS method to CORS configuration

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 app.use(cors({
     origin: ['https://tr-gold.vercel.app', 'http://localhost:5173'],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
