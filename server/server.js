7e2062d2d64ede2adca79a715bd1f9e73a93d5a6
Ansif <99172928+AnsifMkr@users.noreply.github.com>
2026-01-24 09:48:38 +0530

    Add CORS methods and allowed headers configuration

--- server/server.js
+++ server/server.js
@@ -6,7 +6,9 @@ require('dotenv').config();
 
 app.use(cors({
     origin: ['https://tr-gold.vercel.app', 'http://localhost:5173'],
-    credentials: true
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 const mongoose = require('mongoose');
