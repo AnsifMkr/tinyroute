f71a8e2cbcfafd026a17b73c64f2f4950195fa17
Ansif <99172928+AnsifMkr@users.noreply.github.com>
2026-01-24 10:45:31 +0530

    Update CORS methods in server.js
    Removed 'OPTIONS' method from CORS configuration.

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 app.use(cors({
     origin: ['https://tr-gold.vercel.app', 'http://localhost:5173'],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
